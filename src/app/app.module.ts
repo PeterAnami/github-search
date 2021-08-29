c8d016df4ca705460f21f60e317d706ecb5b26ad
peterAnami <peter.andake@student.moringaschool.com>
2021-08-29 18:49:39 +0300

    fast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpperCasePipe } from './upper-case.pipe';
+import { RepositoriesComponent } from './repositories/repositories/repositories.component';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form/search-form.component';
+import { SearchComponent } from './user/search/search.component';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {SearchRequestService} from './search-request.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperCasePipe,
+    RepositoriesComponent,
+    NavbarComponent,
+    SearchFormComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    FormsModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/search-form/search-form/search-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {search} from '../search';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+export class SearchFormComponent implements OnInit {
+  searchInfo = new Search('');
+  @Output() getName = new EventEmitter<Search>();
+
+  searchFor(data){
+      this.getName.emit(data.value.find);
+      console.log(data.value.find)
+      data.reset();
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
